b7637e4ecbb0cd6ef66e3b8d80c59ce36ae13a78
igor <758832+igorovic@users.noreply.github.com>
2024-09-11 15:08:52 +0200

    setup openapi

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,11 +1,42 @@
 import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+import { OpenAPIHono } from '@hono/zod-openapi'
+import { getPlayerByIdRoute, listPlayers } from './routes/player.route'
+import { swaggerUI } from '@hono/swagger-ui'
+import { getAllPlayers, getPlayerById } from './database'
+//  import { Hono } from 'hono'
 
-const app = new Hono()
+// const app = new Hono()
+const app = new OpenAPIHono()
+const api = new OpenAPIHono()
+app.use(cors())
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+// app.get('/', (c) => {
+//   return c.text('Hello Hono!')
+// })
+
+api.openapi(listPlayers, async (c) => {
+  const players = await getAllPlayers()
+  return c.json(players)
 })
+
+api.openapi(getPlayerByIdRoute, async (c) => {
+  const { id } = c.req.valid('param')
+  const player = await getPlayerById(Number(id))
+  return c.json(player)
+})
+
+// The OpenAPI documentation will be available at /doc
+api.doc('/openapi.json', {
+  openapi: '3.0.0',
+  info: {
+    version: '1.0.0',
+    title: 'Table football API',
+  },
+})
+
+app.route('/api', api)
+app.get('/', swaggerUI({ url: '/api/openapi.json' }))
 
 const port = 3022
 console.log(`Server is running on port ${port}`)
---
+++ apps/backend/src/routes/player.route.ts
@@ -0,0 +1,61 @@
+import { createRoute, z } from '@hono/zod-openapi'
+
+const getPlayerByIdSchema = z.object({
+    id: z
+        .string()
+        .min(1)
+        .openapi({
+            param: {
+                name: 'id',
+                in: 'path',
+            },
+            example: '123',
+        }),
+})
+
+const PlayerSchema = z.object({
+    id: z.number().openapi({
+        example: 123,
+    }),
+    name: z.string().openapi({
+        example: 'John Doe',
+    }),
+}).openapi('Player')
+const allPlayersSchema = z.array(PlayerSchema).openapi('Players')
+
+export const listPlayers = createRoute({
+    method: 'get',
+    path: '/players',
+    // request: {
+    //     params: ParamsSchema,
+    // },
+    responses: {
+        200: {
+            content: {
+                'application/json': {
+                    schema: allPlayersSchema,
+                },
+            },
+            description: 'Retrieve the players',
+        },
+    },
+})
+
+
+export const getPlayerByIdRoute = createRoute({
+    method: 'get',
+    path: '/players/{id}',
+    request: {
+        params: getPlayerByIdSchema,
+    },
+    responses: {
+        200: {
+            content: {
+                'application/json': {
+                    schema: z.union([PlayerSchema, z.null()]),
+                },
+            },
+            description: 'Retrieve the player by id',
+        },
+    },
+})
