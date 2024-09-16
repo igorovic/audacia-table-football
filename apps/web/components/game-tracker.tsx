e5da05b1a12a06ba5174ae90b01d1d3b2167b758
igor <758832+igorovic@users.noreply.github.com>
2024-09-16 10:00:42 +0200

    add visual feedback on score save

--- apps/web/components/game-tracker.tsx
+++ apps/web/components/game-tracker.tsx
@@ -1,6 +1,13 @@
 'use client'
-import { ActionIcon, Flex, NumberInput, Stack, Text } from '@mantine/core'
-import { IconMinus, IconPlus } from '@tabler/icons-react'
+import {
+  ActionIcon,
+  Flex,
+  Loader,
+  NumberInput,
+  Stack,
+  Text,
+} from '@mantine/core'
+import { IconCheck, IconMinus, IconPlus } from '@tabler/icons-react'
 import { usePlayers } from '../app/utils/hooks'
 import { useQueryState } from 'nuqs'
 import {
@@ -9,13 +16,34 @@ import {
   setGameGoals,
 } from '../app/utils/mutations'
 import { useMutation } from '@tanstack/react-query'
+import { useEffect, useState } from 'react'
+
+function SavedFeedback() {
+  const [style, setStyle] = useState({
+    opacity: 1,
+    transition: 'opacity 2s linear',
+  })
+  useEffect(() => {
+    setTimeout(() => {
+      setStyle({ opacity: 0, transition: 'opacity 2s linear' })
+    }, 500)
+  })
+  return (
+    <Flex style={style}>
+      <IconCheck style={{ width: 20, height: 20, color: 'green' }} />
+      <Text size="sm" c="green">
+        saved
+      </Text>
+    </Flex>
+  )
+}
 
 function GameTracker() {
   const { player1, player2 } = usePlayers()
   const [g1, setG1] = useQueryState('g1')
   const [g2, setG2] = useQueryState('g2')
   const [gameId] = useQueryState('gameId')
-  const { mutate: incrementG } = useMutation({
+  const { mutate: incrementG, isPending: isIncrementing } = useMutation({
     mutationFn: incrementGameGoals,
     onSuccess: (data) => {
       if (player1?.id === data.playerId) {
@@ -25,7 +53,7 @@ function GameTracker() {
       }
     },
   })
-  const { mutate: decrementG } = useMutation({
+  const { mutate: decrementG, isPending: isDecrementing } = useMutation({
     mutationFn: decrementGameGoals,
     onSuccess: (data) => {
       if (player1?.id === data.playerId) {
@@ -35,7 +63,7 @@ function GameTracker() {
       }
     },
   })
-  const { mutate: setG } = useMutation({
+  const { mutate: setG, isPending: isSetting } = useMutation({
     mutationFn: setGameGoals,
     onSuccess: (data) => {
       if (player1?.id === data.playerId) {
@@ -119,6 +147,18 @@ function GameTracker() {
         >
           <IconPlus />
         </ActionIcon>
+      </Flex>
+      <Flex align="center" justify="center" gap="md" py="md">
+        <Text size="sm">
+          {player1?.name} &nbsp;&nbsp; {g1} - {g2} &nbsp;&nbsp; {player2?.name}
+        </Text>
+        <Flex align="center" justify="center">
+          {isSetting || isIncrementing || isDecrementing ? (
+            <Loader size="xs" color="green" w={20} h={20} />
+          ) : (
+            <SavedFeedback />
+          )}
+        </Flex>
       </Flex>
     </Stack>
   )
